c7b0fe3ba1772fa859381ea525b1f57b5ece9459
Albert Dmitriev <albertdmitriev94@mail.ru>
2022-09-22 18:44:18 +0300

    Calculate3

--- src/AlternativeCalculate/Main.java
+++ src/AlternativeCalculate/Main.java
@@ -20,47 +20,81 @@ public class Main {
          */
         String input = scanner.nextLine();
 
-        int result = Main.myMethod(input);
+        String result = Main.myMethod(input);
 
         System.out.println("Результат: ");
         System.out.println(firstNumber + " " + operation + " " + secondNumber + " = " + result);
 
     }
 
-    static int myMethod(String input) {
+    static String myMethod(String input) {
         /*
          * Убираем пробелы:
          */
         String inputWithoutSpace = input.replaceAll(" ", "");
 
         initOperatorAndNumbers(inputWithoutSpace);
-        converting(firstNumber, secondNumber);
-        checkNumbersIsNoMoreTen(Integer.parseInt(firstNumber), Integer.parseInt(secondNumber));
-        return calculation(numberOneAfterConverting, numberTwoAfterConverting, operation);
+//        converting(firstNumber, secondNumber);
+        if (!isNumber(firstNumber) && !isNumber(secondNumber) && operation == '/') {
+            throw new IllegalStateException("Ошибка при вычислении");
+        }
+
+        if (!isNumber(firstNumber) && !isNumber(secondNumber) && operation == '-') {
+            int indexSecondWord = firstNumber.indexOf(secondNumber);
+            if (indexSecondWord == -1) {
+                return firstNumber;
+            } else {
+                return firstNumber.substring(0, indexSecondWord);
+            }
+        }
+
+
+        if (!isNumber(firstNumber) && isNumber(secondNumber) && operation == '/') {
+            return firstNumber.substring(0, firstNumber.length() / Integer.parseInt(secondNumber));
+        }
+
+        if (!isNumber(firstNumber) && isNumber(secondNumber) && operation == '*') {
+            StringBuilder stringBuilder = new StringBuilder(firstNumber);
+            for (int i = 0; i < Integer.parseInt(secondNumber) - 1; i++) {
+                stringBuilder.append(firstNumber);
+            }
+            return stringBuilder.toString();
+        }
+
+        checkNumbers(firstNumber, secondNumber);
+        String result = calculation(firstNumber, secondNumber, operation);
+
+        if (result.length()>40){
+            return result+"...";
+        }else {
+            return result;
+        }
     }
 
-    private static void converting(String firstNumber, String secondNumberWithTrim) {
-//        if (isRoman) {
-//            numberOneAfterConverting = romanToArabic(firstNumber);
-//            numberTwoAfterConverting = romanToArabic(secondNumberWithTrim);
-//        } else {
-        numberOneAfterConverting = Integer.parseInt(firstNumber);
-        numberTwoAfterConverting = Integer.parseInt(secondNumberWithTrim);
-//        }
-    }
+//    private static void converting(String firstNumber, String secondNumberWithTrim) {
+////        if (isRoman) {
+////            numberOneAfterConverting = romanToArabic(firstNumber);
+////            numberTwoAfterConverting = romanToArabic(secondNumberWithTrim);
+////        } else {
+//        numberOneAfterConverting = Integer.parseInt(firstNumber);
+//        numberTwoAfterConverting = Integer.parseInt(secondNumberWithTrim);
+////        }
+//    }
 
-    public static int calculation(int numberOne, int numberTwo, char operator) {
+    public static String calculation(String firstNumber, String secondNumber, char operator) {
+
+
         return switch (operator) {
-            case '+' -> numberOne + numberTwo;
-            case '-' -> numberOne - numberTwo;
-            case '*' -> numberOne * numberTwo;
-            case '/' -> numberOne / numberTwo;
+            case '+' -> firstNumber + secondNumber;
+//            case '-' -> firstNumber - secondNumber;
+//            case '*' -> firstNumber * secondNumber;
+//            case '/' -> firstNumber / secondNumber;
             default -> throw new IllegalStateException("Ошибка при вычислении");
         };
     }
 
     private static void initOperatorAndNumbers(String input) {
-        char[] arrayOfInputChars = new char[10];
+        char[] arrayOfInputChars = new char[1000];
 
         //итерация для инициализации оператора
         for (int i = 0; i < input.length(); i++) {
@@ -88,14 +122,41 @@ public class Main {
             throw new IllegalArgumentException("Не верный знак операции");
         }
         //инициализация первого и второго числа
-        firstNumber = arrayWithNumbers[0].trim();
-        secondNumber = arrayWithNumbers[1].trim();
+        firstNumber = arrayWithNumbers[0].trim().replaceAll("^\"|\"$", "");
+        secondNumber = arrayWithNumbers[1].trim().replaceAll("^\"|\"$", "");
     }
 
-    public static void checkNumbersIsNoMoreTen(int numberOne, int numberTwo) throws IllegalArgumentException{
-        if ((numberOne < 1) || (numberOne > 10) || (numberTwo < 1) || (numberTwo > 10)) {
-            throw new IllegalArgumentException("Вы ввели значение, неудолетворяющее условию (числа должны быть " +
-                    "целочисленными от 1 до 10)");
+    public static void checkNumbers(String firstNumber, String secondNumber) throws IllegalArgumentException {
+        try {
+
+            if (isNumber(firstNumber) && !isNumber(secondNumber)) {
+                throw new IllegalArgumentException("Первым аргументом выражения, подаваемого на вход, должна быть строка");
+            }
+
+
+            int numberOne = Integer.parseInt(firstNumber);
+            int numberTwo = Integer.parseInt(secondNumber);
+
+            if ((numberOne < 1) || (numberOne > 10) || (numberTwo < 1) || (numberTwo > 10)) {
+                throw new IllegalArgumentException("Вы ввели значение, неудолетворяющее условию (числа должны быть " +
+                        "целочисленными от 1 до 10)");
+            }
+        } catch (NumberFormatException e) {
+            if (firstNumber.length() > 10 || secondNumber.length() > 10) {
+                throw new IllegalArgumentException("Вы ввели значение, неудолетворяющее условию (длина строки должна быть не больше 10)");
+            }
+        }
+    }
+
+    /*
+     * Признак, определяющий то, что пользователь ввел числа
+     */
+    public static boolean isNumber(String number) throws RuntimeException {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (Exception exception) {
+            return false;
         }
     }
 }
--- src/AlternativeCalculate/MyTest.java
+++ src/AlternativeCalculate/MyTest.java
@@ -6,12 +6,10 @@ import org.junit.jupiter.api.Test;
 
 public class MyTest {
     @Test
-    void test0() {
-        String input = "10+3";
-        int result = Main.myMethod(input);
-        Assert.assertEquals(result, 13);
+    void test10() {
+        String input = "hi/acjk";
+        Assert.assertThrows(IllegalStateException.class, () -> Main.myMethod(input));
     }
-
     @Test
     void test7() {
         String input = "11+3";
@@ -23,12 +21,53 @@ public class MyTest {
         String input = "3+word";
         Assert.assertThrows(Exception.class, () -> Main.myMethod(input));
     }
+    @Test
+    void test5() {
+        String input = "word*3";
+        String result = Main.myMethod(input);
+        Assert.assertEquals(result, "wordwordword");
+    }
 
     @Test
-    void test11() {
-        String input = "11^6";
-        Assert.assertThrows(Exception.class, () -> Main.myMethod(input));
+    void test41() {
+        String input = "word/2";
+        String result = Main.myMethod(input);
+        Assert.assertEquals(result, "wo");
     }
+
+    @Test
+    void test42() {
+        String input = "word/1";
+        String result = Main.myMethod(input);
+        Assert.assertEquals(result, "word");
+    }
+
+    @Test
+    void test61() {
+        String input = "word-hi";
+        String result = Main.myMethod(input);
+        Assert.assertEquals(result, "word");
+    }
+
+    @Test
+    void test62() {
+        String input = "wordhi-hi";
+        String result = Main.myMethod(input);
+        Assert.assertEquals(result, "word");
+    }
+
+    @Test
+    void test72() {
+        String input = "wordwordwordwordwordwordwordwordwordwwor+d";
+        String result = Main.myMethod(input);
+        Assert.assertEquals(result, "wordwordwordwordwordwordwordwordwordwword...");
+    }
+//
+//    @Test
+//    void test11() {
+//        String input = "11^6";
+//        Assert.assertThrows(Exception.class, () -> Main.myMethod(input));
+//    }
 
 
 }
