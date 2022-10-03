12e6b6363709dc8fcd15fa01dbe16c1cfca23648
Albert Dmitriev <albertdmitriev94@mail.ru>
2022-10-03 21:16:09 +0300

    FinalCalculate

---
+++ src/StringCalculate/Initializer.java
@@ -0,0 +1,56 @@
+package StringCalculate;
+
+import static StringCalculate.Main.validOperators;
+
+public class Initializer {
+    static String firstOperand, secondOperand;
+    static char operator;
+    static int firstOperandInt, secondOperandInt;
+    String [] arrayWithNumbers;
+    static boolean firstOperandIsInt, secondOperandIsInt = false;
+    public void initOperator(String inputWithoutSpace) {
+        char[] arrayOfInputChars = new char[1000];
+        for (int i = 0; i < inputWithoutSpace.length(); i++) {
+            arrayOfInputChars[i] = inputWithoutSpace.charAt(i);
+            if (arrayOfInputChars[i] == '+') {
+                operator = '+';
+            }
+            if (arrayOfInputChars[i] == '-') {
+                operator = '-';
+            }
+            if (arrayOfInputChars[i] == '*') {
+                operator = '*';
+            }
+            if (arrayOfInputChars[i] == '/') {
+                operator = '/';
+            }
+        }
+        initArrayWithNumbers(arrayOfInputChars, validOperators);
+    }
+
+    public void initArrayWithNumbers(char[] arrayOfInputChars, String validOperators) {
+        arrayWithNumbers = String.valueOf(arrayOfInputChars).split(validOperators);
+    }
+
+    public void initOperand(String[] arrayWithNumbers) {
+        firstOperand = arrayWithNumbers[0].trim().replaceAll("^\"|\"$", "");
+        secondOperand = arrayWithNumbers[1].trim().replaceAll("^\"|\"$", "");
+    }
+
+    public void defineNumbers() {
+        try {
+            firstOperandInt = Integer.parseInt(firstOperand);
+            firstOperandIsInt = true;
+        } catch (Exception exception) {
+            firstOperandIsInt = false;
+        }
+        try {
+            secondOperandInt = Integer.parseInt(secondOperand);
+            secondOperandIsInt = true;
+        } catch (Exception exception) {
+            secondOperandIsInt = false;
+        }
+    }
+}
+
+
